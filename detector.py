6975cf929e1a9bd1a6b6ed412a48f708547d885a
Shorya1835 <shorya_s@mfs.iitr.ac.in>
2023-01-14 18:26:43 +0530

    updated detector code
    100% BAN GAYA!!!! (i think)

--- detector.py
+++ detector.py
@@ -20,10 +20,10 @@ def extractAngle(img):
     returns angle between -1 to 1 corresponding to -90 to 90
     """
     img /= 255.
-    
+
     # break the image into 2 channels (green and redblue)
     # and apply thresholds according to how red+blue or green a pixel is
-
+    
     green = img[:, :, 1]
     redblue = img[:, :, 0] + img[:, :, 2]
 
@@ -38,31 +38,35 @@ def extractAngle(img):
     np.nan_to_num(green, copy=False)
     green = green > greenThresh  # finally `green` is a boolean matrix
 
+    # break matrix into two halves, left for angle, right for shoot
+    w, h = width//2, height
+    lImg = green[:,:w]
+    rImg = green[:,w:]
+
+    #############DETECT ANGLE################
+
     # find the average x and y coordinates and standard deviatons
-    temp = indexarr*green[:, :, None]/np.sum(green)
+    temp = indexarr*lImg[:, :, None]/np.sum(lImg)
     np.nan_to_num(temp, copy=False)
     x, y = temp[:, :, 0].sum(), temp[:, :, 1].sum()
 
-    distx, disty = green.sum(1), green.sum(0)
+    distx, disty = lImg.sum(1), lImg.sum(0)
     stdx, stdy = distx.std(), disty.std()
 
     # print(x, y, '\n', stdx, stdy)
 
     # remove outliers according to how far they are from the mean
-    green[:max(int(y-6*stdy), 0), :]        = False
-    green[min(int(y+6*stdy), height-1):, :] = False
+    size = 4
+    lImg[:max(int(y-size*stdy), 0), :]        = False
+    lImg[min(int(y+size*stdy), h-1):, :] = False
 
-    green[:, :max(int(x-6*stdx), 0)]        = False
-    green[:, min(int(x+6*stdx), width-1)]   = False
+    lImg[:, :max(int(x-size*stdx), 0)]        = False
+    lImg[:,min(int(x+size*stdx), w-1):]   = False
 
     # calculate angle using means and deviations of scatter plot
-    
-##    distx, disty = green.sum(1), green.sum(0)
-##    stdx, stdy = distx.std(), disty.std()
-##    print(stdx/stdy)
 
-    temp = indexarr*green[:, :, None]
-    count = green.sum()
+    temp = indexarr*lImg[:, :, None]
+    count = lImg.sum()
     
     xM, yM = temp[:,:,0].sum()/count, temp[:,:,1].sum()/count
     xyM = (temp[:,:,0]*temp[:,:,1]).sum()/count
@@ -71,8 +75,12 @@ def extractAngle(img):
     angle = -np.arctan((xM*yM - xyM)/(yM**2-y2M))
     if yM**2==y2M or count==0:
         angle = 0
-        
-    return green, 2*angle/np.pi
+
+    #############DETECT SHOOT################
+    print(rImg.sum()/(w*h))
+    shoot = rImg.sum()/(w*h) < 0.002
+
+    return green, 2*angle/np.pi, shoot
 
     # plt.imshow(green, cmap='gray')
     # plt.plot(dist)
@@ -96,15 +104,15 @@ showing, image = video.read()
 # [:,:,0] is x coordinate and [:,:,1] is y coordinate
 height, width = image.shape[:2]
 
-indexarr = np.zeros((height, width, 2))
-indexarr[:, :, 0] = np.arange(width).reshape(1, -1)
+indexarr = np.zeros((height, width//2, 2))
+indexarr[:, :, 0] = np.arange(width//2).reshape(1, -1)
 indexarr[:, :, 1] = np.arange(height).reshape(-1, 1)
 
 while showing:
     try:
-        vision_img, angle = extractAngle(image.astype('float32'))
+        vision_img, angle, shoot = extractAngle(image.astype('float32'))
         
-        print(angle)
+        print(angle, shoot)
         # vision_img = extractAngle(mpimg.imread('test1.jpg')/255.) #####
         
         vision_img = np.repeat(vision_img[:,:,None]*255, repeats=3, axis=2).astype(np.uint8)   #REMOVABLE
@@ -113,6 +121,7 @@ while showing:
         cv2.imshow("actual", image)                                                            #REMOVABLE
         showing, image = video.read()
         image = cv2.flip(image, 1)
+        image[:, width//2-10:width//2+10 :] = [0,0,255]
         
         
         # exit if escape pressed
